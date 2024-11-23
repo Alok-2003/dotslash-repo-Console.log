e2f0fc52c0abccb2df134794eab38ad6d6d7f2a6
Alok-2003 <alok953280@gmail.com>
2024-11-23 11:15:59 +0530

    Basic setup done

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-centre font-bold text-3xl w-full' >
+        <h1 className='text-center' >Login Page</h1> 
+    </div>
+  )
+}
+
+export default page
---
+++ app/signup/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-centre font-bold text-3xl w-full' >
+        <h1 className='text-center' >Sign Up Page</h1> 
+    </div>
+  )
+}
+
+export default page
